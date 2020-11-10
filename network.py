ac67741591ae725b53f3a00169418bf7a6d96350
thunderingwest4 <aaravgupta04@gmail.com>
2020-11-10 11:53:45 -0500

    added ShiftRight function in tensorflow encoded, havent tested if it works yet

--- network.py
+++ network.py
@@ -1,9 +1,11 @@
 from tensorflow.keras import layers as tfl
 import tensorflow.keras as tk
 import tensorflow as tf
-import tensorflow.lattice as tflat
+#import tensorflow.lattice as tflat
 import numpy as np
-import extraLayers as el
+
+# Architecture from: https://towardsdatascience.com/understanding-neural-machine-translation-encoder-decoder-architecture-80f205643ba4
+# Bahdanau Attention: https://blog.floydhub.com/attention-mechanism/ 
 
 class NMTAttn():
 
@@ -16,7 +18,7 @@ class NMTAttn():
 
             # get the Select thing working - [0,1,0,1]
 
-            tflat.ParallelCombination(inp_encoder, pre_attention_dec), 
+            #tflat.ParallelCombination(inp_encoder, pre_attention_dec), 
             tfl.Lambda(function=pre_attn_inp, output_shape=4), 
             tfl.Residual(tfl.Attention(d_model, n_attn_heads, dropout, mode)),
 
@@ -29,22 +31,22 @@ class NMTAttn():
 
         )
 
-        return model
+        self.model = model
 
-    def input_encoder_fn(input_vocab_size, d_model, n_encoder) -> tk.Sequential:
+    def input_encoder_fn(self, input_vocab_size, d_model, n_encoder) -> tk.Sequential:
         return tk.Sequential(
             tfl.Embedding(input_vocab_size, d_model), 
             [tfl.LSTM(d_model) for i in range(n_encoder)]
         )
 
-    def pre_attn_decoder(mode, target_vocab_size, d_model) -> tk.Sequential:
+    def pre_attn_decoder(self, mode, target_vocab_size, d_model) -> tk.Sequential:
         return tk.Sequential(
-            tfl.ShiftRight(mode=mode), 
+            ShiftRight(mode=mode), 
             tfl.Embedding(target_vocab_size, d_model), 
             tfl.LSTM(d_model)
         )
 
-    def pre_attn_inp(encoder_activ, decoder_activ, inps) -> (np.array, np.array, np.array, np.array):
+    def pre_attn_inp(self, encoder_activ, decoder_activ, inps) -> (np.array, np.array, np.array, np.array):
         keys = encoder_activ
         vals = encoder_activ
 
@@ -56,4 +58,28 @@ class NMTAttn():
 
         return queries, keys, vals, mask
 
+
+"""
+SHIFTRIGHT AND _ZERO_PAD METHOD COPIED FROM TRAX GITHUB
+"""
+def _zero_pad(x, pad, axis):
+  """Helper for jnp.pad with 0s for single-axis case."""
+  pad_widths = [(0, 0)] * len(x.shape)
+  pad_widths[axis] = pad  # Padding on axis.
+  return np.pad(x, pad_widths, mode='constant',
+                 constant_values=x.dtype.type(0))
+
+def ShiftRight(n_positions=1, mode='train'):
+  """Returns a layer that can insert padding to shift the input sequence.
+  Args:
+    n_positions: Number of positions to shift the input sequence rightward;
+        initial positions freed by the shift get padded with zeros.
+    mode: One of `'train'`, `'eval'`, or `'predict'`.
+  """
+  def f(x):
+    if mode == 'predict':
+      return x
+    padded = _zero_pad(x, (n_positions, 0), 1)
+    return padded[:, :-n_positions]
     
+  return tfl.Lambda(name=f'ShiftRight({n_positions})', function=f)
